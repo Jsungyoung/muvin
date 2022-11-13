996ed17e4109f99a5b2b4d55324e0e4e751d9ab7
User <sungyoung961@naver.com>
2022-11-13 18:56:27 +0900

    first commit

---
+++ src/test/java/com/example/muvin/MuvinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.muvin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuvinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
