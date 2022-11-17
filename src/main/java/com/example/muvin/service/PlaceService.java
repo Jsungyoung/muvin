6c8c81b582c3146b521bae1bb842a7dc5c282f22
User <sungyoung961@naver.com>
2022-11-17 20:21:52 +0900

    update : 2022/11/14 11:42

--- src/main/java/com/example/muvin/service/PlaceService.java
+++ src/main/java/com/example/muvin/service/PlaceService.java
@@ -1,14 +1,11 @@
 package com.example.muvin.service;
 
 import com.example.muvin.domain.place.Place;
-import com.example.muvin.domain.place.PlaceDto;
 import com.example.muvin.domain.place.PlaceRepository;
-import com.example.muvin.entity.Place_review;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
