768cc02d7891004e86fe2df929a53cd07c6cfab9
hanseung-u <scox1090@gmail.com>
2022-11-20 18:04:51 +0900

    카카오 유저 회원가입 처리중

--- src/main/resources/static/javascript/kakaoLogin.js
+++ src/main/resources/static/javascript/kakaoLogin.js
@@ -1,17 +1,56 @@
 const REST_API_KEY = "6a4c077fca80e32384fcf93a071c6788";
-const REDIRECT_URI = "https://localhost:8087";
-let code = "";
+let code = new URL(window.location.href).searchParams.get("code");
+if(code!=null){
+    kakaoLogin();
+}
 
 function kakaoLogin(){
-    let url = "https://kauth.kakao.com/oauth/token";
-    url += "?grant_type=authorization_code";
-    url += `&client_id${REST_API_KEY}`;
-    url += `&redirect_uri=${REDIRECT_URI}`;
-    url += `&code=H0kHpwpjqaozBCwzNUzdpjW2neK36RaR93hBbhVYpHZJgYgnS798rPBJ7voH0EIZQ_rypQo9cxcAAAGEjvYhgA`
-    url += "&client_secret=MbVu8Zn9GcUQcp6fY04ydBR6aoqO8Sho"
+    console.log(code);
+    if(code!=null) {
+        $.ajax({
+            method: "POST",
+            url: "https://kauth.kakao.com/oauth/token",
+            header: {
+                contentType: "application/x-www-form-urlencoded;charset=utf-8"
+            },
+            data: {
+                grant_type : "authorization_code",
+                client_id: REST_API_KEY,
+                redirect_url: "http://localhost:8084/loginForm",
+                code: code
+            }
+        }).done(function (res) {
+            console.log(res.access_token);
+            getUserInfo(res.access_token)
+        });
+    }
+}
+
+function getUserInfo(access_token){
     $.ajax({
-        method : "POST",
-        url : url,
-        contentType : "application/x-www-form-urlencoded;charset=utf-8"
+        method: "GET",
+        url: "https://kapi.kakao.com/v2/user/me?porperty",
+        headers: {
+            Authorization : `Bearer ${access_token}`
+        }
+    }).done(function (res) {
+        let id = res.id;
+        let email = res.kakao_account.email;
+        let nickname = res.kakao_account.profile.nickname;
+        $.ajax({
+            async: false,
+            method: "POST",
+            url: "/v1/kakao/login",
+            data: {
+                id: id
+            }
+        }).done(function(res) {
+            console.log(res);
+            if(res==="/signUpForm"){
+                let data = `{"id":"${id}","nickname":"${nickname}","email":"${email}"}`;
+                sessionStorage.setItem("data",data);
+            }
+            location.replace(res);
+        });
     });
 }
